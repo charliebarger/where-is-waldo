a5fe232ea13f13432c2876000de0784e20187f3a
charliebarger <charliebarger04@gmail.com>
2021-09-24 16:10:34 -0600

    add theme

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import GolbalStyles from "./styles/globalStyles";
 import Header from "./header";
 import GameImage from "./GameImage";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 import db from "./firebase.config";
+
 import { collection, addDoc } from "firebase/firestore";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GolbalStyles />
       <Header />
       <GameImage />
-    </>
+    </ThemeProvider>
   );
 }
 
