e91d4313447b7e6be01bbaecae081f97eb353199
charliebarger <charliebarger04@gmail.com>
2021-09-25 10:50:41 -0600

    initial commit

---
+++ src/NavItems.js
@@ -0,0 +1,31 @@
+import styled, { css } from "styled-components";
+
+const NavItem = styled.span`
+  display: inline-block;
+  color: white;
+  font-family: Calligraphr;
+  font-size: 26px;
+  &:hover {
+    transform: scale(1.1);
+    -webkit-text-stroke: 1px ${({ theme }) => theme.colors.yellow};
+    color: ${({ theme }) => theme.colors.blue};
+    text-shadow: 0px 0px 5px ${({ theme }) => theme.colors.green};
+  }
+  ${({ mobile }) =>
+    mobile &&
+    css`
+      text-align: center;
+      font-size: 30px;
+      margin: 20px;
+    `}
+`;
+
+const LeftNavItem = styled(NavItem)`
+  margin-left: auto;
+`;
+
+const RightNavItem = styled(NavItem)`
+  margin: 0px 50px;
+`;
+
+export { NavItem, LeftNavItem, RightNavItem };
