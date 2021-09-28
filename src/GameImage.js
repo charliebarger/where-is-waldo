d61822478f45b602710205a7149616f116030a95
charliebarger <charliebarger04@gmail.com>
2021-09-28 17:50:37 -0600

    add custom cursor

--- src/GameImage.js
+++ src/GameImage.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 const StyledImage = styled.img`
   width: 100%;
+  cursor: url(https://cdn.custom-cursor.com/db/cursor/32/rick_and_morty_rick_sanchez_laser_gun_cursor.png),
+    default !important;
 `;
 const GameImage = () => {
   return (
