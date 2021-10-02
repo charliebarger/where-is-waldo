2da20d60d7e48be8236c3b72cb05388708542123
charliebarger <charliebarger04@gmail.com>
2021-10-02 12:16:25 -0600

    add turn phone pop up

--- src/componenets/body/GameBody.js
+++ src/componenets/body/GameBody.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameImage from "./GameImage";
 import styled from "styled-components";
 import PopUpWrapper from "../pop-ups/PopUpWindow";
 import SideCharacters from "./SideCharacters";
+import TurnPhone from "./TurnPhone";
 const Body = styled.div`
   overflow-x: hidden;
   width: 100%;
@@ -12,8 +13,25 @@ const Body = styled.div`
 `;
 
 const GameBody = ({ slide, setSlide }) => {
+  const [turnPhoneAlert, setTurnPhoneAlert] = useState(false);
+  const [Xclicked, setXclicked] = useState(false);
+
+  const closePopUp = (width) => {
+    if (width < 400 && !Xclicked) {
+      setTurnPhoneAlert(true);
+    } else {
+      setTurnPhoneAlert(false);
+    }
+  };
+  useEffect(() => {
+    closePopUp(window.screen.width);
+    console.log("hi");
+    window.addEventListener("resize", (e) => closePopUp(e.target.innerWidth));
+  }, [Xclicked]);
+
   return (
     <Body>
+      {turnPhoneAlert && <TurnPhone setClose={setXclicked}></TurnPhone>}
       <SideCharacters slide={slide} />
       <GameImage slide={slide} />
       <PopUpWrapper slide={slide} setSlide={setSlide}></PopUpWrapper>
---
+++ src/componenets/body/TurnPhone.js
@@ -0,0 +1,97 @@
+import React from "react";
+import styled, { css, keyframes } from "styled-components";
+
+const rotatePhone = keyframes`
+0% {
+		transform: rotate(0deg)
+	}
+  
+  50% {
+		transform: rotate(0deg)
+	}
+	
+	100% {
+		transform: rotate(90deg)
+	}
+`;
+
+const Phone = styled.div`
+  height: 200px;
+  width: 100px;
+  backdrop-filter: saturate(180%) blur(10px);
+  border: black solid 5px;
+  border-bottom: black solid 25px;
+  border-top: black solid 25px;
+  border-radius: 10px;
+  background: white;
+  position: relative;
+  animation: ${rotatePhone} 1.5s ease-in-out infinite alternate;
+`;
+
+const PhoneWrapper = styled.div`
+  padding: 20px;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  border: black solid 2px;
+  border-radius: 10px;
+  background: #ffffffad;
+`;
+
+const BlurBackground = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  backdrop-filter: blur(10px);
+`;
+
+const Message = styled.span`
+  font-size: 22px;
+`;
+
+const Exit = styled.div`
+  cursor: pointer;
+  margin-left: auto;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  &:before,
+  &:after {
+    background: black;
+    content: "";
+    display: block;
+    width: 100%;
+    height: 5px;
+  }
+  &:before {
+    transform: translateY(2.5px) rotate(135deg);
+  }
+
+  &:after {
+    transform: translateY(-2.5px) rotate(-135deg);
+  }
+`;
+
+const TurnPhone = ({ setClose }) => {
+  return (
+    <BlurBackground>
+      <PhoneWrapper>
+        <Exit onClick={() => setClose(true)} />
+        <Phone></Phone>
+        <Message>Please Rotate Your Device!</Message>
+      </PhoneWrapper>
+    </BlurBackground>
+  );
+};
+
+export default TurnPhone;
